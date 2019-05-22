4b99f096be9428fc07138383dc07c59c8a156c86
Ed Webber <edwebber06@gmail.com>
2019-05-22 10:42:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(line)
   else
   current_line = "The line is currently:"
   line.each.with_index do |name, index|
-  current_line.push("#{index}. #{name}")
+  current_line.push("#{index+1}. #{name}")
 end
 puts current_line
 end
